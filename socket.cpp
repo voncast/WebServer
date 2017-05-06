c7139ec10d34530ea6b41f18a4f7173406c73493
Alice Yang <1538857261@qq.com>
2017-05-06 10:19:39 +0800

    a perfect web server with 404(use qfile instead of qpixmap)

--- socket.cpp
+++ socket.cpp
@@ -21,36 +21,45 @@ void socket::rcv(){
 //    qDebug()<<"read:"<<rcvMessage.data();
     QString str;
     str.prepend(rcvMessage);
+
     QString pattern("GET\\s(.*)\\sHTTP");
     QString pat("Host:(.*)Connection");
     QRegExp regg(pat);
     regg.indexIn(str);
     QString reqHost=regg.capturedTexts()[1];
+
     QString pat1("(.*)HTTP");
     QRegExp reg1(pat1);
     reg1.indexIn(str);
     QString reqHead=reg1.capturedTexts()[1];
+
     QString reqSent=reqHost+"   "+reqHead;
     int i=id;
     emit sendReq(reqSent,i);
+
     QRegExp reg(pattern);
     reg.indexIn(str);
     in=reg.capturedTexts()[1];
     qDebug()<<reg.capturedTexts()[1];
+
     sendTo();
 }
 void socket::sendTo(){
     qDebug()<<"send";
-    QString dir,sta="200 OK";
-    dir=index;
-    QString type,str;
+    QString dir=index;
+    QString sta="200 OK";
+    QString type="text/html";
+    QString str;
     myResponse*res=new myResponse();
-    type="text/html";
+
+    //匹配文件后缀名
     QString pat("\.([^\.]+)$");
     QRegExp reg(pat);
     reg.indexIn(in);
     QString fileType=reg.capturedTexts()[1];
     qDebug()<<"Gdgdggd:"<<reg.capturedTexts()[1];
+
+    //确定响应报文中的content-type
     if(!in.contains(".")){
         if(!in.compare("/")){
             in+="index.html";
@@ -59,35 +68,21 @@ void socket::sendTo(){
         }
     }else if(in.contains("png")||in.contains("jpg")||in.contains("jpeg")||in.contains("ico")||in.contains("gif")){
         type="image/"+fileType;
-//        type+="image/";
-//        type+=in.contains("png")?"png":"jpg";
     }else if(fileType.contains("css")){
         type="text/css";
     }else if(fileType.contains("js")){
         type="application/javascript";
     }
+
+    //打开文件
     dir+=in;
     qDebug()<<"dir:"<<dir;
     QFile f(dir);
     QBuffer buf;
     QByteArray line;
-    if(type.contains("image")){
-//        sta="200 OK";
-//        res->addStatus(sta);
-//        res->addType(type);
-        QPixmap pix(dir);
-        buf.open(QIODevice::ReadWrite);
-        QString t=type.contains("png")?"png":"jpg";
-        QByteArray ba = t.toLatin1();
-        char *mm = ba.data();
-        pix.save(&buf,mm);
-        line.append(buf.data());
-        if(line.size()==0){
-            sta="404 Not Found";
-        }
-    }else if(type.contains("text")||type.contains("javascript")){
-    if(!f.open(QIODevice::ReadOnly | QIODevice::Text))
+    if(!f.open(QIODevice::ReadOnly))
     {
+        //打不开则返回404
        qDebug() << "Open failed." << endl;
        QString rdir=QCoreApplication::applicationDirPath();
        rdir+="/src/404.html";
@@ -98,23 +93,21 @@ void socket::sendTo(){
            QByteArray li = fr.readLine();
            line.append(li);
        }
-        sta="404 Not Found";
-//        res->addStatus(sta);
+       sta="404 Not Found";
     }else{
-//        res->addStatus(sta);
-//        res->addType(type);
         while(!f.atEnd()) {
             QByteArray l = f.readLine();
             line.append(l);
         }
     }
-    }
-//    QString qs(line);
+    //往响应报文中添加信息
     res->addStatus(sta);
     res->addType(type);
+    //计算返回内容的长度
     QString s=QString::number(line.size(),10);
     qDebug()<<"size:"<<s;
     res->addLength(s);
+    //给ui界面展示状态
     if(sta.contains("404")){
         emit sendResStatus(false,s,id);
     }else{
@@ -122,72 +115,6 @@ void socket::sendTo(){
     }
     this->write(res->response().toStdString().c_str());
     this->write(line);
-//    QString size=QString::number(line.size(),10);
-//    res->addLength(size);
-//    qDebug()<<res->response();
-//    this->write(res->response().toStdString().c_str());
-//    this->write(line);
 }
 
-//void socket::sendTo(){
-//    qDebug()<<"send";
-//    QString dir=QCoreApplication::applicationDirPath();
-//    dir+="/src";
-//    dir=index;
-//    QDateTime date=QDateTime::currentDateTime();
-//    QString d=date.toString("ddd, dd MMM yyyy hh:mm:ss ");
-//    d+="GMT";
-//    QString sendMessage="HTTP/1.1 200 OK\r\nDate:"+d+"\r\n";
-//    QString str,type;
-//    QBuffer buf;
-//    QByteArray data;
-//    bool fimg=false;
-//    if(!in.compare("/")||!in.compare("/index")){
-//        dir+="/index.html";
-//    }else if(!in.compare("/page")){
-//        dir+="/page.html";
-//    }
-//    else if(in.contains("png")||in.contains("jpg")){
-//        dir+=in;
-//        QPixmap pix(dir);
-//        buf.open(QIODevice::ReadWrite);
-//        type=in.contains("png")?"png":"jpg";
-//        QByteArray ba = type.toLatin1();
-//        char *mm = ba.data();
-//        pix.save(&buf,mm);
-//        fimg=true;
-//    }
-//    if(!fimg){
-//        sendMessage+="Content-type:text/html\r\n";
-//        qDebug()<<"dir:"<<dir;
-//        QFile f(dir);
-//        if(!f.open(QIODevice::ReadOnly | QIODevice::Text))
-//        {
-//            qDebug() << "Open failed." << endl;
-//            return;
-//        }
-//        QTextStream input(&f);
-//        while(!input.atEnd()){
-//            str+=input.readLine();
-//        }
-//        QString size=QString::number(str.length(),10);
-//        sendMessage+="Content-length:"+size+"\r\n\r\n";
-//        sendMessage+=str;
-//        qDebug()<<"sent-length:"<<sendMessage.length();
-//    //    soc->connectToHost(QHostAddress("127.0.0.1"), port);
-//        this->write(sendMessage.toStdString().c_str());
-//    }else{
-//        sendMessage+="Accept-Ranges:bytes\r\n";
-//        sendMessage+="Content-type:image/"+type+"\r\n";
-//        data.append(buf.data());
-//        QString qs(data);
-//        QString s=QString::number(data.length(),10);
-//        qDebug()<<"size:"<<s;
-//        sendMessage+="Content-length:"+s+"\r\n\r\n";
-//        this->write(sendMessage.toStdString().c_str());
-//        this->write(data);
-//    }
-
-
-//}
 
